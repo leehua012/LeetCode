e81d987677fa0e356e1b782f1143ec444e4b9860
lhGoh <66687235+leehua012@users.noreply.github.com>
2022-07-18 11:28:41 +0800

    Update solution.js

--- JavaScript/905-Sort Array By Parity/solution.js
+++ JavaScript/905-Sort Array By Parity/solution.js
@@ -1,1 +1,20 @@
-
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    if(nums.length<=1)return nums;
+    
+    let i=0;
+    let j=0;
+    
+    while(j<nums.length){
+        if(nums[j]%2 == 0){
+            temp = nums[i];
+            nums[i++] =  nums[j];
+            nums[j] = temp;
+        }
+        j++;
+    }
+    return nums;
+};
