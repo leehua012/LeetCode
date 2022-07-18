cc9fe8a860453aa67fd3de063f219e0b239a1fab
lhGoh <66687235+leehua012@users.noreply.github.com>
2022-07-18 12:29:38 +0800

    Create solution-v2.js

---
+++ JavaScript/27-Remove Element/solution-v2.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let i = 0;
+    let j = nums.length;
+    
+    while(i<j){
+        if(nums[i] == val){
+            nums[i] = nums[j-1];
+            j--;
+        }else{
+            i++;
+        }
+    }
+    return j;
+};
