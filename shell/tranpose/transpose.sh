7609d86729c0dbe3b0079136ac177e56e4104a96
lhGoh <66687235+leehua012@users.noreply.github.com>
2024-04-04 23:30:30 +0800

    Create transpose.sh

---
+++ shell/tranpose/transpose.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+awk '
+{
+    for (i=1; i<=NF; i++) {
+        if(NR==1) {
+            transposed[i] = $i;
+        } else {
+            transposed[i] = transposed[i] " " $i;
+        }
+    }
+}
+END {
+    for (i=1; i<=NF; i++) {
+        print transposed[i];
+    }
+}' file.txt
